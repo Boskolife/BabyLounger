e83097080278b76c5444561e9812bf8565c400d0
Boskolife <94291571+Boskolife@users.noreply.github.com>
2024-08-08 16:41:09 +0300

    add hidden search

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 initBurger();
+openSearch();
 
 function initBurger() {
   const burger = document.querySelector('.burger_menu');
@@ -15,5 +16,33 @@ function initBurger() {
     burger.classList.remove('burger_active');
     menu.classList.remove('menu_active');
     document.body.classList.remove('body_lock');
+  });
+}
+
+
+function openSearch() {
+  const searchWrapper = document.querySelector('.search_wrap');
+  const searchBar = document.querySelector('.search_bar');
+  const searchBtn = document.querySelector('.search_icon');
+  const searchInput = document.getElementById('search_input');
+
+  searchBtn.addEventListener('click', () => {
+    event.stopPropagation();
+    searchBtn.classList.add('searchBtn_active');
+    searchWrapper.classList.add('searchWrapper_active');
+    searchInput.classList.add('searchInput_active');
+    searchBar.classList.add('searchBar_active');
+  });
+
+  document.addEventListener('click', (event) => {
+    if (
+      !searchWrapper.contains(event.target) &&
+      !searchBtn.contains(event.target)
+    ) {
+      searchBtn.classList.remove('searchBtn_active');
+      searchWrapper.classList.remove('searchWrapper_active');
+      searchInput.classList.remove('searchInput_active');
+      searchBar.classList.remove('searchBar_active');
+    }
   });
 }
