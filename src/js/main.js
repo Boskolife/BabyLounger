3e5a231aa4bb11ebcbd4ab5c7b67223d4e24fdf3
Boskolife <94291571+Boskolife@users.noreply.github.com>
2024-08-08 15:46:01 +0300

    Add header menu mob

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,19 @@
 initBurger();
 
-
 function initBurger() {
   const burger = document.querySelector('.burger_menu');
   const menu = document.querySelector('.menu');
+  const returnBtn = document.querySelector('.return_btn');
 
-  burger.addEventListener('click', (item) => {
+  burger.addEventListener('click', () => {
     burger.classList.toggle('burger_active');
     menu.classList.toggle('menu_active');
+    document.body.classList.toggle('body_lock');
+  });
+
+  returnBtn.addEventListener('click', () => {
+    burger.classList.remove('burger_active');
+    menu.classList.remove('menu_active');
+    document.body.classList.remove('body_lock');
   });
 }
